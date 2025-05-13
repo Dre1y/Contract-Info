fdef01ada2c2c2111b847b37f7661c45ba39e90e
Andrey <andreymatheus92@gmail.com>
2025-05-13 20:22:10 -0300

    fix: update foreign key constraints to reference attachment table instead of deliverable

--- api/src/main/resources/db/migration/V4__add_contract_id_to_attachment.sql
+++ api/src/main/resources/db/migration/V4__add_contract_id_to_attachment.sql
@@ -1,7 +1,7 @@
-ALTER TABLE deliverable
+ALTER TABLE attachment
     ADD COLUMN contract_id BINARY(16);
 
-ALTER TABLE deliverable
-    ADD CONSTRAINT fk_deliverable_contract
+ALTER TABLE attachment
+    ADD CONSTRAINT fk_attachment_contract
         FOREIGN KEY (contract_id)
             REFERENCES contract(id);
